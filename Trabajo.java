0278f66d02d6bf43d8e60c69c5c42db9ee8be1fd
TonyVHT <160373875+TonyVHT@users.noreply.github.com>
2024-02-18 21:10:43 -0600

    Commit trabajo archivo

---
+++ Trabajo.java
@@ -0,0 +1,66 @@
+public class Trabajo{
+    public static void main(String []args){
+        //Mau
+
+
+
+
+
+
+
+
+
+
+
+        //Tony
+
+
+
+
+
+
+        
+        
+    }
+
+    //Funciones Mau
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Funciones Tony
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
